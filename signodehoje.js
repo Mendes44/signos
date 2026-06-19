52c8af9652741f138ccca7dc931eca9ddd455b3d
Mendes44 <marcosmendesm10@gmail.com>
2026-06-20 02:02:09 +1000

    Corrigindo Bug no Mês de Capricornio

--- signodehoje.js
+++ signodehoje.js
@@ -22,14 +22,23 @@ const retorna_signo = (signos, data) => {
         let data_inicio_signo = new Date(ano + "-" + signo["DataInicio"] + " 00:00:00",);
         let data_fim_signo = new Date(ano + "-" + signo.DataFim + " 23:59:59"); //Aqui posso colocar signos[0]["DataFim"] ou .DataFim
         
-        if (data >= data_inicio_signo && data <= data_fim_signo) {
-            return signo["Nome"];
+        let tipo_comparacao = signo.DataInicio == "12-22" ? "or" : "and";
+
+
+        if (tipo_comparacao == "and"){
+            if (data >= data_inicio_signo && data <= data_fim_signo) {
+                return signo["Nome"];
+            }
+        }else if (tipo_comparacao == "or"){
+            if (data >= data_inicio_signo || data <= data_fim_signo){
+                return signo.Nome;
+            }
         }
     }
 };
 
 
-let data_app = new Date("2026-07-28 00:00:00");//ano-mes-dia
+let data_app = new Date("2026-12-30 00:00:00");//ano-mes-dia
 const nome_signo = retorna_signo(signos, data_app);
 
 console.log("O signo de hoje é: " + nome_signo);
